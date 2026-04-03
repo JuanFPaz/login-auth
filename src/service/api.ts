8fa7e0842b56574e03312f790dce4edbccdc3af0
Juan Paz <jpaz.dev94@gmail.com>
2026-04-03 05:28:49 -0300

    Moviendo api.ts a modulo service

---
+++ src/service/api.ts
@@ -0,0 +1,37 @@
+// const DEV_URL = 'http://localhost:3000'
+const PROD_URL = "https://jwt-prueba.onrender.com";
+import type { userRegister, userLogin } from "../types/typeService";
+import { optionsGET, optionsPOST } from "../utils/options";
+
+async function api<T>(url: string, options: RequestInit): Promise<T> {
+  const res: Response = await fetch(PROD_URL + url, options);
+  if (!res.ok) {
+    if (res.status === 404) {
+      const err: any = await res.json();
+      throw new Error(`${err.status} - ${err.message}`);
+    }
+
+    throw new Error(`${res.status} - ${res.statusText}`);
+  }
+  return res.json() as T;
+}
+
+async function post<T>(url: string, options: RequestInit) {
+  return await api<T>(url, options);
+}
+
+export async function getUser<T>(url: string): Promise<T> {
+  return await api<T>(url, optionsGET());
+}
+
+export async function postLogin<T>(url: string, body: userLogin) {
+  return await post<T>(url, optionsPOST(body));
+}
+
+export async function postSignUp<T>(url: string, body: userRegister) {
+  return await post<T>(url, optionsPOST(body));
+}
+
+export async function postLogut<T>(url: string): Promise<T> {
+  return await post<T>(url, optionsPOST());
+}
